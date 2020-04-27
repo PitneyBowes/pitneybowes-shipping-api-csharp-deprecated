e419a4681b22e2a879a2bebffd1d78efd4590861
NEX7FDK <neha.biyani@pb.com>
2020-04-27 13:47:18 +0530

    Create label for domestic Shipment

--- examples/MyShip/MyShip.cs
+++ examples/MyShip/MyShip.cs
@@ -10,7 +10,7 @@ namespace MyShip
 {
     class Program
     {
-         static void Main(string[] args)
+        static void Main(string[] args)
         {
             var sandbox = new Session()
             {
@@ -22,7 +22,7 @@ namespace MyShip
                     { "ApiKey", "9DugRoGkf4rvvlMfnlxumj3ggzzWDCWY" },
                     { "ApiSecret", "IY5tGLbnNAV7dYH5" },
                     { "RatePlan", "YOUR_RATE_PLAN" },
-                   /// { "ShipperID", "3000107147" },
+                    { "ShipperID", "3000107147" },
                  ///   { "DeveloperID", "58172307" }
                 };
             var configurationBuilder = new ConfigurationBuilder();
@@ -34,59 +34,63 @@ namespace MyShip
             Model.RegisterSerializationTypes(sandbox.SerializationRegistry);
             Globals.DefaultSession = sandbox;
             var shipment = (Shipment)ShipmentFluent<Shipment>.Create()
-                
                           .ToAddress((Address)AddressFluent<Address>.Create()
                           .Company("Shop")
-                          .Name("Manager")
+                          .Name("Mary Jones")
                           .Phone("620-555-0000")
-                          .Email("shop@example.com")
+                          .Email("mary@example.com")
 
-                             .AddressLines("643 Greenway RD")
-                              .CityTown("Boone").StateProvince("NC")
-                              .PostalCode("28607")
+                             .AddressLines("284 W Fulton")
+                              .CityTown("Garden City").StateProvince("KS")
+                              .PostalCode("67846")
                               .CountryCode("US")
                               )
                          .FromAddress((Address)AddressFluent<Address>.Create()
                               .Company("Supplies")
-                               .Name("Manager")
-                               .Phone("415-555-0000")
-                               .Email("supplies@example.com")
-                              .AddressLines("545 Market St")
-                              .CityTown("San Francisco").StateProvince("CA").PostalCode("94105")
+                               .Name("Kathryn Smith")
+                               .Phone("334-000-0000 ")
+                               .Email("kathryn@example.com")
+
+
+
+
+                              .AddressLines("2352 Bent Creek Rd")
+                              .CityTown("Auburn").StateProvince("AL").PostalCode("36830")
                               .CountryCode("US")
                               )
-                         .Parcel((Parcel)ParcelFluent<Parcel>.Create().
-                              Dimension(6, 4, 1, UnitOfDimension.IN)
-                              .Weight(38, UnitOfWeight.OZ))
+                         .Parcel((Parcel)ParcelFluent<Parcel>.Create()
+                              ///.Dimension(12, 0.25M, 9)
+                              .Weight(20, UnitOfWeight.OZ))
                          .Rates(RatesArrayFluent<Rates>.Create().
                               Add().Carrier(Carrier.FEDEX)
-                              ///.Service(Services.TwoDA)
+                               .Service(Services.TwoDA)
                               .ParcelType(ParcelType.PKG))
+                       ///  .SpecialService<ISpecialServices>(SpecialServiceCodes.INS,0M,new Parameter() { Name = "INPUT_VALUE", Value = "0" })))
 
+                       /// .SpecialService<SpecialServices>(SpecialServiceCodes.INS, 0M, new Parameter("INPUT_VALUE", "50")))
 
-                        ///  .SpecialService<ISpecialServices>(SpecialServiceCodes.INS,0M,new Parameter() { Name = "INPUT_VALUE", Value = "0" })))
+                       .Documents((List<IDocument>)DocumentsArrayFluent<Document>.Create()
+                              .ShippingLabel(ContentType.URL, Size.DOC_4X6, FileFormat.PDF))
 
-                        /// .SpecialService<SpecialServices>(SpecialServiceCodes.INS, 0M, new Parameter("INPUT_VALUE", "50")))
-
-                        ///  .Documents((List<IDocument>)DocumentsArrayFluent<Document>.Create()
-                        ///      .ShippingLabel(ContentType.URL, Size.DOC_4X6, FileFormat.PDF))
-
-                        ///  .ShipmentOptions(ShipmentOptionsArrayFluent<ShipmentOptions>.Create()
-                        ////      .ShipperId(sandbox.GetConfigItem("ShipperID"))
-                        /// .MinimalAddressvalidation().AddOption(ShipmentOption.PERMIT_NUMBER, "SHFL")
-                        /// )
-                        ///
-                        
-                        .TransactionId(Guid.NewGuid().ToString().Substring(15));
-
-            shipment.CarrierAccountId = "323832c1-0646-4c99-80f5-7a7fe97ecdd9";
+                         .ShipmentOptions(ShipmentOptionsArrayFluent<ShipmentOptions>.Create()
+                              .ShipperId(sandbox.GetConfigItem("ShipperID"))
+                              /// .MinimalAddressvalidation().AddOption(ShipmentOption.PERMIT_NUMBER, "SHFL")
+                              )
+                         .TransactionId(Guid.NewGuid().ToString().Substring(15));
 
             shipment.IncludeDeliveryCommitment = true;
             var shipmentreq = (Shipment)shipment;
             shipmentreq.IncludeDeliveryCommitment = true;
             ///shipmentreq.CarrierAccountId= "323832c1-0646-4c99-80f5-7a7fe97ecdd9";
-            var label = Api.Rates(shipment).GetAwaiter().GetResult(); ;
-           
+            var label = Api.CreateShipment(shipment).GetAwaiter().GetResult();
+            if (label.Success)
+            {
+                var sw = new StreamWriter("label.png");
+                foreach (var d in label.APIResponse.Documents)
+                {
+                    Api.WriteToStream(d, sw.BaseStream).GetAwaiter().GetResult();
+                }
+            }
 
         }
     }
